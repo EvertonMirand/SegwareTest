04f9e5ba39cb204a23ef21bcdba87a6466ed9027
EvertonMirand <everton.mirandav@gmail.com>
2020-08-04 20:42:01 -0300

    Criação de utilitarios

---
+++ src/routes/RoutesContants.ts
@@ -0,0 +1,5 @@
+const LOGIN = 'Login';
+const FEED = 'Feed';
+const SIGN_UP = 'SignUp';
+
+export { LOGIN, FEED, SIGN_UP };
---
+++ src/utils/Colors.ts
@@ -0,0 +1,21 @@
+const PRIMARY_COLOR = '#0d47a1';
+const BUTTON = PRIMARY_COLOR;
+const TAB_BAR_COLOR = PRIMARY_COLOR;
+const BACKGROUND = '#fff';
+const TEXT_FIELD = '#002171';
+const BUTTON_TEXT = '#fff';
+const ERROR_TEXT = '#ff0033';
+const BORDER = '#5472d3';
+const LINK = '#82b1ff';
+
+export {
+  BUTTON,
+  BACKGROUND,
+  TEXT_FIELD,
+  BUTTON_TEXT,
+  ERROR_TEXT,
+  TAB_BAR_COLOR,
+  BORDER,
+  PRIMARY_COLOR,
+  LINK,
+};
